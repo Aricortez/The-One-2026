d7bb311d64679416cbebc1b05931c35ff989a8c5
Aricortez <darinkacortez4@gmail.com>
2026-05-07 21:37:37 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
 const CACHE_NAME = 'theone-v1';
-const assets = ['./', './index.html'];
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.44.0/tabler-icons.min.css'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
